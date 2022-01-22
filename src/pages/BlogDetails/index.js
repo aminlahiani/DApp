feec3930afd8e70f44ffd7ff25648301da2437ab
aminlahiani <aminlahiani@outlook.com>
2022-01-22 09:48:04 +0100

    fix some change

--- src/pages/BlogDetails/index.js
+++ src/pages/BlogDetails/index.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { Details } from "./sections/Details";
-import { Hero } from "./sections/Hero";
-
-
-import { useParams } from "react-router-dom";
-import BlogSideBar from "./sections/BlogSidebar";
-import { Container, Grid, Typography } from "@mui/material";
-//import RecentPostCard from "components/Cards/RecentPostCard";
+import BlogDetailsHero from "components/BlogDetails/BlogDetailsHero";
+import BlogDetails from "components/BlogDetails/BlogDetails";
 import RecentPostsList from "components/BlogDetails/RecentPostsList";
 
+import { useParams } from "react-router-dom";
+import { Container, Grid } from "@mui/material";
+//import RecentPostCard from "components/Cards/RecentPostCard";
+
 import BlogsData from "data/Blogs/blogs-data.json";
+
 function index() {
   const parameter = useParams();
   const postId = parameter.postId;
   const data = BlogsData.filter((blog) => blog.id === postId);
-  const data1 = [1, 2, 3, 4, 5, 6];
+
   return (
     <div>
-      <Hero />
+      <BlogDetailsHero />
+
       <Container maxWidth="lg">
         <Grid container spacing={3}>
           <Grid item md={8} xs={12}>
-            <Details data={data[0]} />
+            <BlogDetails data={data[0]} />
           </Grid>
           <Grid item md={4} xs={12}>
             <RecentPostsList data={BlogsData} />
