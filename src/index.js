33fbecc06ef7d5109f9fad3b4be1b0e634c28bf5
aminlahiani <aminlahiani@outlook.com>
2022-01-23 08:59:23 +0100

    fix theme config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
+
 import App from "./App";
-import { SettingsConsumer, SettingsProvider } from "./contexts/settings-context";
-import { createTheme } from "./theme";
-import AppThemeProvider from "theme1/AppThemeProvider";
+
+import AppThemeProvider from "theme/AppThemeProvider";
 
 ReactDOM.render(
   <AppThemeProvider>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <App />
-          </AppThemeProvider>,
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </AppThemeProvider>,
   document.querySelector("#root")
 );
--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,33 +1,22 @@
-import React from "react";
-import { useEffect, useState } from "react";
-
-import { Box, ThemeProvider } from "@mui/material";
-import { useSettings } from "../hooks/use-settings";
-
-import { createTheme } from "../theme";
 import { Outlet } from "react-router-dom";
-import { MainFooter } from "../components/Footers/MainFooter";
+import { MainFooter } from "components/Footers/MainFooter";
 import MainNavbar from "components/Navbars/MainNavbar";
+import { Box } from "@mui/material";
 function MainLayout() {
-
-
-
   return (
     <>
-    
-        {/* <ButtonAppBar/> */}
-        <Box
-          sx={{
-            backgroundColor: "background.default",
-            // paddingTop: '59px'
-          }}
-        >
-          <MainNavbar />
+      {/* <ButtonAppBar/> */}
+      <Box
+        sx={{
+          backgroundColor: "background.default",
+          // paddingTop: '59px'
+        }}
+      >
+        <MainNavbar />
 
-          <Outlet />
-          <MainFooter />
-        </Box>
-      
+        <Outlet />
+        <MainFooter />
+      </Box>
     </>
   );
 }
