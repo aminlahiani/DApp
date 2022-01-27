90bcebc1b4a04acdb8ac302353e09ef0ac4d2cc4
aminlahiani <aminlahiani@outlook.com>
2022-01-27 17:10:08 +0100

    fix e

--- src/components/Navbars/MainNavbar.js
+++ src/components/Navbars/MainNavbar.js
@@ -55,7 +55,7 @@ const MainNavbar = ({ setSelectedTheme, selectedTheme }) => {
     >
       <Container maxWidth="lg">
         <Toolbar sx={{ height: 59 }} disableGutters>
-          <Box component="img" src="/landingpage.svg" alt="macbook" height="30px" />
+          <Box component="img" src="/landingpage.svg" alt="macbook" height="30px" width="30px"   />
 
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
