78278e8208a1415b8025a686943c0a4c32f4a253
aminlahiani <aminlahiani@outlook.com>
2022-01-30 11:24:15 +0100

    fix some change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppThemeProvider from "theme/AppThemeProvider";
+import reportWebVitals from "reportWebVitals";
 import App from "./App";
 
 ReactDOM.render(
@@ -12,3 +13,8 @@ ReactDOM.render(
   </AppThemeProvider>,
   document.querySelector("#root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
